bea2f932a276a4d5918da85f2b72dda7c37f876b
Dionathan Cordova <dionathan.cordova@vivitech.com.br>
2020-05-30 14:58:46 -0300

    teste de appointments pronto

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -4,18 +4,17 @@ import CreateAppointmentService from './CreateAppointmentService';
 describe('CreateAppointment', () => {
     it('should be able to create a new appointment', async () => {
         const fakeAppointmentRepository = new FakeAppointmentRepository();
-        // const createAppointment = new CreateAppointmentService(
-        //     fakeAppointmentRepository,
-        // );
+        const createAppointment = new CreateAppointmentService(
+            fakeAppointmentRepository,
+        );
 
-        // const appointment = await createAppointment.execute({
-        //     provider_id: '123456',
-        //     date: new Date(),
-        // });
+        const appointment = await createAppointment.execute({
+            provider_id: '123456',
+            date: new Date(),
+        });
 
-        // expect(appointment).toHaveProperty('id');
-        // expect(appointment.id).toBe('123456');
-        expect(1 + 2).toBe(3);
+        expect(appointment).toHaveProperty('id');
+        expect(appointment.provider_id).toBe('123456');
     });
 
     it('should not be able to create two appointments on the same date', () => {
